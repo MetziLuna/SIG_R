2c218803e4f5affd5f11fda314ec8f8421434b46
MetziLuna <48043166+MetziLuna@users.noreply.github.com>
2021-06-24 23:22:39 -0600

    actualización de comentarios

--- mapa_sismosSV.R
+++ mapa_sismosSV.R
@@ -1,5 +1,5 @@
 # Copyright (C) Metzi Aguilar
-# This file is part of dogtag <https://github.com/MetziLuna/SIG_R/>.
+# This file is part of SIG_R <https://github.com/MetziLuna/SIG_R/>.
 #
 # SIG_R is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,19 +12,19 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with dogtag.  If not, see <http://www.gnu.org/licenses/>.
+# along with SIG_R.  If not, see <http://www.gnu.org/licenses/>.
 
 library(leaflet)
 library(tidyr)
 library(dplyr)
-
+#lectura de sismos de El Salvador ocurridos en un período de 10 años. Fuente: Ministerio de Medio Ambiente de El Salvador (MARN)
 df_sismos <- 
   read.csv(
     "https://raw.githubusercontent.com/BigDreamsCoders/Tremor/master/static/Sismos-el-salvador.csv")
-# first 20 quakes
+# first 100 quakes
 df.100 <- df_sismos[1:100,]
 
-
+#Obtención de la latitud, longitud y magnitud de los sismos
 lat = df.100$Latitud.N...
 long = df.100$Longitud.W...
 mag = df.100$Magnitud
@@ -32,15 +32,13 @@ mag = df.100$Magnitud
 
 mutate(df.100, group = cut(mag, breaks = c(0, 4, 5, Inf), labels = c("azul", "verde", "rojo"))) -> df.100
 
-### I edit this png file and created my own marker.
-### https://raw.githubusercontent.com/lvoogdt/Leaflet.awesome-markers/master/dist/images/markers-soft.png
+### iconos creados a partir de la imagen https://raw.githubusercontent.com/lvoogdt/Leaflet.awesome-markers/master/dist/images/markers-soft.png
 quakeIcons <- iconList(azul = makeIcon("https://raw.githubusercontent.com/MetziLuna/Img/main/icono_azul.png", iconWidth = 24, iconHeight =32),
                        verde = makeIcon("https://raw.githubusercontent.com/MetziLuna/Img/main/icono_verde.png", iconWidth = 24, iconHeight =32),
                        rojo = makeIcon("https://raw.githubusercontent.com/MetziLuna/Img/main/icono_rojo.png", iconWidth = 24, iconHeight =32))
 
-
 leaflet(df.100) %>% addTiles() %>% setView(-89.09389,13.998889, zoom = 8)%>%
-  addTiles() %>%
-  addMarkers(~long, ~lat, icon = ~quakeIcons[group])
+addTiles() %>%
+addMarkers(~long, ~lat, icon = ~quakeIcons[group])
 
 
