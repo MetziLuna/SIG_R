b5c970c86494f332f7f712796f2c3fe192338c5b
MetziLuna <48043166+MetziLuna@users.noreply.github.com>
2021-06-24 23:18:16 -0600

    actualización de comentarios

--- mapa_cuantitativo.R
+++ mapa_cuantitativo.R
@@ -1,5 +1,5 @@
 # Copyright (C) Metzi Aguilar
-# This file is part of dogtag <https://github.com/MetziLuna/SIG_R/>.
+# This file is part of SIG_R <https://github.com/MetziLuna/SIG_R/>.
 #
 # SIG_R is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,27 +12,27 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with dogtag.  If not, see <http://www.gnu.org/licenses/>.
+# along with SIG_R.  If not, see <http://www.gnu.org/licenses/>.
 
 ############################## PLOT
-
+#Carga de librerías necesarias
 library(tidyr)
 library(dplyr)
 library(sf)
 
-# Data frame de casos activos por cantón, con fechas en las columnas
-# Lectura de la capa de cantones de un archivo GeoJSON
+# Data frame de municipios
+# Lectura de la capa de municipios de El Salvador desde un archivo GeoJSON
 sf_municipios <-
   st_read(
     "https://raw.githubusercontent.com/MetziLuna/Datos_SV/main/municipios.geojson", 
     quiet = T
   )
-# Seleccionamos paleta de 7 colores de Color rojo
+# Seleccionamos paleta de 6 colores de Color rojo
 library(RColorBrewer)
 pal <- brewer.pal(6, "OrRd") 
 class(pal)
 
-# Mapeo cuantitativo de la población usando método Cuantiles con 7 clases
+# Mapeo cuantitativo de la población usando método Cuantiles con 6 clases
 plot(sf_municipios["densidad"], main = "Población total por municipio 2007",
      breaks = "quantile", 
      nbreaks = 6,
@@ -44,6 +44,7 @@ plot(sf_municipios["densidad"], main = "Población total por municipio 2007",
 summary(sf_municipios["densidad"])
 
 ############################## GGPLOT
+# Mapa cuantitativo con paquete Ggplot
 library(ggplot2)
 ggplot(sf_municipios) + 
   geom_sf(aes(fill=densidad))
@@ -62,22 +63,21 @@ ggplot(sf_municipios) +
   scale_fill_brewer(palette = "OrRd")
 
 ## Mapa estático con GGMAP
-
+# Mapa cuantitativo usando el paquete Ggmap
 library(ggmap)
 
 us <- c(left = -90.233, bottom = 13, right = -87.5, top = 14.456)
-#bbox <- expand_bbox(st_bbox(nc_centers), X = -89.09389, Y =  13.998889)
-#location=c(lon = -89.09389, lat = 13.998889)
+
 ph_basemap <- get_stamenmap(us, zoom=8, maptype = 'terrain', source = 'osm')
 
 #  Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.
-#ggmap(ph_basemap)
+
 ggmap(ph_basemap) +
   geom_sf(data = sf_municipios, aes(alpha = 0.8, fill=densidad_cat), inherit.aes = FALSE) +
   scale_fill_brewer(palette = "OrRd") +   coord_sf(crs = st_crs(4326))
 
 ############################## TMAP
-
+# Mapas cuantitativos usando el paquete Tmap y diferentes métodos de clasificación
 library(tmap)
 tm_shape(sf_municipios) +
   tm_polygons("densidad", 
